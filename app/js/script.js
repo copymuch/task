3dc90607313150bf3ce82ed36ce031cc101d07ca
copymuch <87070708+copymuch@users.noreply.github.com>
2021-07-21 13:38:51 +0700

    upd2

--- app/js/script.js
+++ app/js/script.js
@@ -2,10 +2,12 @@ let button1 = document.getElementById('bttn1');
 let buttons = document.querySelectorAll('button');
 let content1 = document.querySelector('.content-block_1');
 let content2 = document.querySelector('.content-block_2');
+let text = document.getElementById('text');
 
 content1.style.backgroundColor="red";
 
 button1.onclick = function(){
 	content1.style.backgroundColor = "blue";
 	content2.style.display = "none";
+	text.innerHTML = "Один блок";
 }
